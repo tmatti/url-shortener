ff5a4d3418d210a96451d4ed4362a01d17bae2cb
tmatti <tim.matti@careerbuilder.com>
2019-10-04 15:10:17 -0400

    added show action

--- url-shortener-api/app/controllers/shortened_urls_controller.rb
+++ url-shortener-api/app/controllers/shortened_urls_controller.rb
@@ -3,6 +3,12 @@ class ShortenedUrlsController < ApplicationController
     @urls = ShortenedUrl.all
     render json: @urls
   end
+
+  def show
+    @shortened_url = ShortenedUrl.find_by_slug(params[:slug])
+    render json: @shortened_url
+  end
+
   def create
     @shortened_url = ShortenedUrl.create!(shortened_url_params)
     render json: @shortened_url
