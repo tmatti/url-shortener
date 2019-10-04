26dd0b3d120009ef549b719f6f7dcec5c77c472e
tmatti <tim.matti@careerbuilder.com>
2019-10-04 13:15:46 -0400

    added basic create action

--- url-shortener-api/app/controllers/shortened_urls_controller.rb
+++ url-shortener-api/app/controllers/shortened_urls_controller.rb
@@ -1,2 +1,11 @@
 class ShortenedUrlsController < ApplicationController
+  def create
+    @url = ShortenedUrl.create(shortened_url_params)
+    render json: @url
+  end
+
+  private
+  def shortened_url_params
+    params.require(:shortened_url).permit(:url)
+  end
 end
