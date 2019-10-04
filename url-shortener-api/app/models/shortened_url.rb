e6e3106e16dda18ad3b007afd1f7aec65933deae
tmatti <tim.matti@careerbuilder.com>
2019-10-04 15:21:05 -0400

    clean up url on create

--- url-shortener-api/app/models/shortened_url.rb
+++ url-shortener-api/app/models/shortened_url.rb
@@ -1,10 +1,17 @@
 class ShortenedUrl < ApplicationRecord
   before_validation :generate_slug, on: :create
-
+  before_validation :cleanse_url, on: :create
   validates :redirect_url, presence: true, format: { with: URI.regexp }
   validates :slug, presence: true, uniqueness: true
 
   private
+
+  def cleanse_url
+    unless self.redirect_url.starts_with?('http')
+      self.redirect_url = "http://#{self.redirect_url}"
+    end
+  end
+
   def generate_slug
     # this gives us over 44 billion options. 62 choose 6 = n!/(n-k)! = 62!/56!
     charset = ['A'..'Z','a'..'z','0'..'9'].map{|range| range.to_a}.flatten
