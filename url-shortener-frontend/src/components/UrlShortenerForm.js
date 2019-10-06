6d5e9fcdfcade9197e4912a8648a0c505d8de24e
tmatti <tim.matti@careerbuilder.com>
2019-10-06 16:28:35 -0400

    added ability to create new shortenedUrls and very basic error messaging

--- url-shortener-frontend/src/components/UrlShortenerForm.js
+++ url-shortener-frontend/src/components/UrlShortenerForm.js
@@ -1,29 +1,73 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 
 class UrlShortenerForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      urls: []
+      urls: [],
+      value: '',
+      error: '',
     }
   }
+
+  handleChange = (event) => {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit = (event) => {
+    axios.post('http://localhost:3000/shortened_urls',
+    {
+      redirect_url: this.state.value
+    }
+    )
+    .then(response => {
+      this.setState({
+        urls: this.state.urls.concat(response.data),
+        error: '',
+      })
+    })
+    .catch(error => {
+      this.setState({error: error.message});
+    });
+    event.preventDefault();
+  }
+
   render() {
+    const urls = this.state.urls.slice();
+    const urlList = urls.map(url => {
+      const link = 'http://localhost:3000/' + url.slug;
+      return (
+        <li key={url.id}>
+          <a
+            target="_blank"
+            href={link}>
+            {link}
+          </a>
+        </li>
+      );
+    });
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
+        {this.state.error}
         <fieldset>
           <input
             type="text"
             placeholder="Shorten your link"
             className="shorten-input"
             autoComplete="off"
+            value={this.state.value}
+            onChange={this.handleChange}
           />
           <input
-            type="button"
+            type="submit"
             value="Shorten"
             className="shorten-button"
           />
         </fieldset>
+        <div className="url-list">
+          {urlList}
+        </div>
       </form>
     )
   }
